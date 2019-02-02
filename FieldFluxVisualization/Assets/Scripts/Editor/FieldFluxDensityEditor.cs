a6d58b3f63941ed43096ec29de6f71c76c4213cc
grgsiberia <nanashi4129@gmail.com>
2019-02-02 19:57:23 +0900

    Particleを制御するシステムをどうしたものか

---
+++ FieldFluxVisualization/Assets/Scripts/Editor/FieldFluxDensityEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FieldFluxDensityData))]
+public class FieldFluxDensityEditor : Editor
+{
+    FieldFluxDensityLoader loader;
+    string path = "";
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.LabelField("CSV File");
+        EditorGUILayout.BeginHorizontal();
+        path = EditorGUILayout.TextField(path);
+
+        if (GUILayout.Button("Open"))
+        {
+            path = EditorUtility.OpenFilePanel("Read CSV File", "", "csv");
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Import CSV"))
+        {
+            loader = new FieldFluxDensityLoader(path);
+        }
+    }
+}
